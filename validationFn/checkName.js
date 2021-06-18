ab592be75e04fa577386b492dc0e9e6654dc8410
GrzegorzNecka <strzypior@gmail.com>
2021-06-18 14:36:00 +0200

    1 file modified

--- validationFn/checkName.js
+++ validationFn/checkName.js
@@ -1,9 +1,11 @@
 function checkName(nodeElem) {
   const val = nodeElem.value;
-
+  const regex = /[a-ząęńółńżź]/gm;
   if (val.length <= 2) {
     console.log('za mało znaków');
   }
+
+  console.log(regex.test(val));
 
   console.dir(val.length);
 }
